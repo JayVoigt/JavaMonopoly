d233ef1f6967a01c8c53ce79f8b0e861143ef8e3
jayvoigt <jayvoigt@iu.edu>
2022-03-21 19:01:29 -0400

    updated documentation

--- src/main/java/GameLogicController.java
+++ src/main/java/GameLogicController.java
@@ -77,15 +77,26 @@ public class GameLogicController implements Serializable {
 		return gameLogContents;
 	}
 
+	/**
+	 * Send a welcome message to the game log when the application is first launched.
+	 */
 	public void sendWelcomeMessage() {
 		appendToGameLog("Welcome to Java Monopoly Prototype!");
 		appendToGameLog("A new game can be started or loaded under the File menu.\n");
 	}
 
+	/**
+	 * Send a message to the game log when the game has started, including information about the number of players specified.
+	 */
 	public void sendInitGameMessage() {
 		appendToGameLog("A new game has been started with " + playersCount + " players.");
 	}
 
+	/**
+	 * Append the input to the game log as a formatted line.<br>
+	 * Entries are prefixed with the current date and time, as well as the current turn number.<br>
+	 * @param input The contents of the message.
+	 */
 	public void appendToGameLog(String input) {
 		Date currentDate = new Date();
 		SimpleDateFormat datePrefix = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
@@ -101,6 +112,10 @@ public class GameLogicController implements Serializable {
 		return debugLogContents;
 	}
 
+	/**
+	 * Append the input to the debug log.
+	 * @param input The contents of the message.
+	 */
 	public void appendToDebugLog(String input) {
 		String formattedPrefix = Integer.toString(turnCounter);
 		formattedPrefix = formattedPrefix.concat(": ");
@@ -108,6 +123,10 @@ public class GameLogicController implements Serializable {
 	}
 	// </editor-fold>
 
+	/**
+	 * Evaluator which is run at the beginning of a player's turn.<br>
+	 * Implements which decision path to execute given the player's condition.
+	 */
 	public void initialEvaluator() {
 		appendToDebugLog("-> executing initialEvaluator");
 		turnCounter++;
@@ -129,12 +148,18 @@ public class GameLogicController implements Serializable {
 		}
 	}
 
+	/**
+	 * Evaluator which is run when the current player is currently in Jail.
+	 */
 	private void jailStateEvaluator() {
 		appendToDebugLog("-> executing jailStateEvaluator");
 		appendToGameLog(currentPlayer.getCustomName() + " is jailed!");
 
 	}
 
+	/**
+	 * Evaluator which is run if no special conditions apply to the current player.
+	 */
 	private void normalTurnEvaluator() {
 		appendToDebugLog("-> executing normalTurnEvaluator");
 		if (currentPlayer.hasRolledDice == true) {
@@ -147,6 +172,12 @@ public class GameLogicController implements Serializable {
 		}
 	}
 
+	/**
+	 * Evaluator which is run when the player is in mandatory actions exhausted (MAE) state.<br>
+	 * This occurs when the player has no remaining mandatory actions, such as rolling the dice
+	 * or ending their turn, but they are still permitted to perform optional actions, such as
+	 * mortgaging a property or building improvements.
+	 */
 	private void maeStateEvaluator() {
 		appendToDebugLog("-> executing maeStateEvaluator");
 		currentPlayer.setActionLockedEndTurn(false);
@@ -181,12 +212,20 @@ public class GameLogicController implements Serializable {
 		}
 	}
 
+	/**
+	 * Manager which is executed when the player decides to purchase a property.
+	 */
 	private void propertyPurchaseManager() {
 		appendToDebugLog("-> executing propertyPurchaseManager");
 		currentPlayer.setRequiredDecisionPropertyAction(false);
 		currentPlayer.setMadeDecisionPropertyAction(true);
 	}
 
+	/**
+	 * Manager which is executed when the current player rolls the dice.<br>
+	 * Note: this does not execute for special dice roll conditions, such as rolling
+	 * for doubles when jailed, or rolling to determine the rent paid when landing on an owned Utility.
+	 */
 	public void diceRollManager() {
 		appendToDebugLog("-> executing diceRollManager");
 		currentPlayer = board.players.get(board.getCurrentPlayerID());
@@ -220,6 +259,9 @@ public class GameLogicController implements Serializable {
 		}
 	}
 
+	/**
+	 * Ready the state of the current player to be jailed; jail the player.
+	 */
 	private void jailPlayer() {
 		currentPlayer.setIsJailed(true);
 		currentPlayer.setHasRolledDice(true);
@@ -228,6 +270,10 @@ public class GameLogicController implements Serializable {
 		currentPlayer.setInitialJailTurn(true);
 	}
 
+	/**
+	 * Evaluator which executes when the current player rolls the dice, and a roll quantity is determined.<br>
+	 * Determines where the player should move, and then executes the appropriate evaluator for the destination space.
+	 */
 	private void movementEvaluator() {
 		appendToDebugLog("-> executing movementEvaluator");
 		int diceSum = currentPlayer.getDiceSum();
@@ -253,6 +299,13 @@ public class GameLogicController implements Serializable {
 		maeStateEvaluator();
 	}
 
+	/**
+	 * An advanced evaluator, derived from <code>movementEvaluator</code>, which executes when more precise
+	 * movement of a player's position is required.<br>
+	 * This occurs when a player requires a <code>teleportRelative</code> event from <code>DrawCard</code> to be executed.
+	 * @param collectGoBonus Whether the player should collect a bonus if they pass or land on GO.
+	 * @param movementQuantity The number of spaces to move.
+	 */
 	private void movementEvaluatorAdvanced(boolean collectGoBonus, int movementQuantity) {
 		appendToDebugLog("-> executing movementEvaluatorAdvanced");
 		boolean playerPassedGo = currentPlayer.advancePosition(movementQuantity);
@@ -277,6 +330,9 @@ public class GameLogicController implements Serializable {
 		maeStateEvaluator();
 	}
 
+	/**
+	 * Evaluator which executes when the landed space is of <code>GameEvent</code> type.
+	 */
 	private void gameEventEvaluator() {
 		appendToDebugLog("-> executing gameEventEvaluator");
 
@@ -311,6 +367,9 @@ public class GameLogicController implements Serializable {
 		maeStateEvaluator();
 	}
 
+	/**
+	 * Evaluator which executes when the landed space is of <code>DrawCard</code> type.
+	 */
 	private void drawCardEvaluator() {
 		int randomCardID = (int) (Math.random() * board.chanceCards.size());
 		String gameLogDrawPrefix;
@@ -360,6 +419,9 @@ public class GameLogicController implements Serializable {
 		}
 	}
 
+	/**
+	 * Evaluator which executes when the landed space is of <code>Property</code> type.
+	 */
 	private void propertyEvaluator() {
 		appendToDebugLog("-> executing propertyEvaluator");
 		currentProperty = (Property) currentSpace;
@@ -383,6 +445,9 @@ public class GameLogicController implements Serializable {
 		}
 	}
 
+	/**
+	 * Manager which executes when the current player ends their turn.
+	 */
 	public void endTurnManager() {
 		appendToDebugLog("-> executing endTurnManager");
 		currentPlayer = board.players.get(board.getCurrentPlayerID());
@@ -406,6 +471,9 @@ public class GameLogicController implements Serializable {
 		initialEvaluator();
 	}
 
+	/**
+	 * Decision evaluator which executes when the current player decides to purchase a property.
+	 */
 	public void playerDecisionPurchaseProperty() {
 		appendToDebugLog("-> executing playerDecisionPurchaseProperty");
 		currentPlayer.setMadeDecisionPropertyAction(true);
@@ -427,10 +495,17 @@ public class GameLogicController implements Serializable {
 		}
 	}
 
+	/**
+	 * Decision evaluator which executes when the player decides to send a landed property to auction.<br>
+	 * <b>Not implemented.</b>
+	 */
 	public void playerDecisionAuction() {
 		appendToDebugLog("-> executing playerDecisionAuction");
 	}
 
+	/** 
+	 * Decision evaluator which executes when the current player decides to post bail when jailed.
+	 */
 	public void playerDecisionJailPostBail() {
 		currentPlayer.setMadeDecisionPostedBail(true);
 		if (currentPlayer.getCurrentBalance() >= 50) {
@@ -443,6 +518,9 @@ public class GameLogicController implements Serializable {
 		}
 	}
 
+	/**
+	 * Decision evaluator which executes when the current player decides to roll for doubles when jailed.
+	 */
 	public void playerDecisionJailRollDoubles() {
 		currentPlayer.setMadeDecisionPostedBail(true);
 		currentPlayer.rollDice();
@@ -467,6 +545,9 @@ public class GameLogicController implements Serializable {
 		}
 	}
 
+	/**
+	 * Ready the state of the current player to be released from jail; release the player from jail.
+	 */
 	private void readyPlayerForJailRelease() {
 		currentPlayer.initializePlayerForNewTurn();
 
@@ -479,6 +560,10 @@ public class GameLogicController implements Serializable {
 		currentPlayer.setActionLockedRollDice(false);
 	}
 
+	/**
+	 * Manager which is executed when the player decides to forfeit the game.<br>
+	 * <b>Not implemented.</b>
+	 */
 	public void forfeitManager() {
 
 	}
