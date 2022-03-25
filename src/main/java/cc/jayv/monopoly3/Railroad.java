d3699f50c1c1dd58c82673474d73a71d0613355e
jayvoigt <jayvoigt@iu.edu>
2022-03-25 12:52:39 -0400

    railroad rent evaluation working

--- src/main/java/cc/jayv/monopoly3/Railroad.java
+++ src/main/java/cc/jayv/monopoly3/Railroad.java
@@ -14,20 +14,44 @@ import java.util.Map;
  */
 public class Railroad extends Property implements Serializable {
 
+	int ownedRailroads;
+	int rentRailroad2,
+		rentRailroad3,
+		rentRailroad4;
+	
 	public Railroad(Map<String, Integer> propertyAttributes, int inputSpaceID, String inputFriendlyName) {
 		super(null);
 		id = inputSpaceID;
 		friendlyName = inputFriendlyName;
 
 		purchaseCost = propertyAttributes.get("purchaseCost");
-		rentHouse1 = propertyAttributes.get("rentHouse1");
-		rentHouse2 = propertyAttributes.get("rentHouse2");
-		rentHouse3 = propertyAttributes.get("rentHouse3");
-		rentHouse4 = propertyAttributes.get("rentHouse4");
+		rentBase = propertyAttributes.get("rentBase");
+		rentRailroad2 = propertyAttributes.get("rentHouse1");
+		rentRailroad3 = propertyAttributes.get("rentHouse2");
+		rentRailroad4 = propertyAttributes.get("rentHouse3");
 	}
 
 	@Override
 	public int calculateRent() {
-		return 0;
+		if (ownedRailroads == 2) {
+			return rentRailroad2;
+		}
+		else if (ownedRailroads == 3) {
+			return rentRailroad3;
+		}
+		else if (ownedRailroads == 4) {
+			return rentRailroad4;
+		}
+		else {
+			return rentBase;
+		}
+	}
+	
+	public int getOwnedRailroads() {
+		return ownedRailroads;
+	}
+	
+	public void setOwnedRailroads(int input) {
+		ownedRailroads = input;
 	}
 }
