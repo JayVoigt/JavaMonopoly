315ccdf3a3ca3e17228cb8f6f5cfc05fe5d06bb8
jayvoigt <jayvoigt@iu.edu>
2022-03-30 16:56:25 -0400

    dynamic prompt generator improved

--- src/main/java/cc/jayv/monopoly3/DialogCreator.java
+++ src/main/java/cc/jayv/monopoly3/DialogCreator.java
@@ -1,22 +1,11 @@
 package cc.jayv.monopoly3;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import javax.swing.BorderFactory;
-import javax.swing.Box;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -55,7 +44,7 @@ public class DialogCreator {
 		JDialog userPrompt = new JDialog();
 		MigLayout promptMigLayout = new MigLayout("fill");
 		userPrompt.setLayout(promptMigLayout);
-		userPrompt.setUndecorated(true);
+		//userPrompt.setUndecorated(true);
 		
 		JLabel titleLabel = new JLabel();
 		JTextArea infoArea = new JTextArea();
@@ -89,11 +78,21 @@ public class DialogCreator {
 		
 		dialogButtonList.get(0).setText("long text long text");
 		
-		for (JButton b : dialogButtonList) {
-			String migButtonSpec = "cell " + dialogButtonList.indexOf(b) + " 3";
-			b.addActionListener(testListener());
-			userPrompt.add(b, migButtonSpec);
-			b.setVisible(true);
+//		for (JButton b : dialogButtonList) {
+//			String migButtonSpec = "cell " + dialogButtonList.indexOf(b) + " 3";
+//			b.addActionListener(testListener());
+//			userPrompt.add(b, migButtonSpec);
+//			b.setVisible(true);
+//		}
+		
+		for (ButtonContents bc : dialogButtonContentList) {
+			bc.addActionListener(testListener());
+			if (bc.getMigLayoutSpec().isEmpty()) {
+				userPrompt.add(bc);
+			}
+			else {
+				userPrompt.add(bc, bc.getMigLayoutSpec());
+			}
 		}
 		
 		return userPrompt;
@@ -111,34 +110,31 @@ public class DialogCreator {
 		return testListener;
 	}
 	
-	public static class ButtonContents {
-		String buttonText;
-		Icon buttonIcon;
+	public static class ButtonContents extends JButton {
 		String buttonAction;
+		String customMigLayoutSpec;
 		
-		public ButtonContents(String buttonText, String buttonIconResource, String buttonAction) {
-			this.buttonText = buttonText;
+		public ButtonContents(String buttonText, String buttonIconResource, String buttonAction, String customMigLayoutSpec) {
+			this.setText(buttonText);
+			this.customMigLayoutSpec = customMigLayoutSpec;
 			
-			URL iconResouce = getClass().getResource(buttonIconResource);
 			if (buttonIconResource.isEmpty() == false) {
-				this.buttonIcon = new ImageIcon(getClass().getResource(buttonIconResource));
+				this.setIcon(new ImageIcon(getClass().getResource(buttonIconResource)));
 			}
 			else {
-				this.buttonIcon = null;
+				this.setIcon(null);
 			}
 			this.buttonAction = buttonAction;
+			
+			this.setVisible(true);
 		}
 		
-		public String getText() {
-			return buttonText;
-		}
-		
-		public Icon getIcon() {
-			return buttonIcon;
-		}
-		
-		public String getAction() {
+		public String getBCAction() {
 			return buttonAction;
+		}
+		
+		public String getMigLayoutSpec() {
+			return customMigLayoutSpec;
 		}
 	}
 	
@@ -150,7 +146,7 @@ public class DialogCreator {
 		ArrayList<ButtonContents> contentList2 = new ArrayList<>();
 
 		for ( int i = 0 ; i < 3 ; i++ ) {
-			ButtonContents bc = new ButtonContents(Integer.toString(i), "", "none");
+			ButtonContents bc = new ButtonContents(Integer.toString(i), "", "none", "");
 			contentList.add(bc);
 		}
 		
@@ -159,9 +155,12 @@ public class DialogCreator {
 		localDialog.setVisible(true);
 		localDialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		
-		ButtonContents bc = new ButtonContents("Example text", "/red-x.png", "close");
+		ButtonContents bc = new ButtonContents("Example text", "/red-x.png", "close", "");
 		contentList2.add(bc);
-		bc = new ButtonContents("short", "/money.png", "money");
+		bc = new ButtonContents("short", "/money.png", "money", "wrap");
+		contentList2.add(bc);
+		
+		bc = new ButtonContents("longer", "/red-x.png", "long", "span, pushx, growx");
 		contentList2.add(bc);
 		
 		JDialog localDialog2 = creator2.createDialogUserPrompt(contentList2);
