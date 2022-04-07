2780cd499107db29693e69069b8ab15dafea4a48
jayvoigt <jayvoigt@iu.edu>
2022-04-07 14:11:41 -0400

    updated space selection area

--- src/main/java/cc/jayv/monopoly3/SpaceSelectionArea.java
+++ src/main/java/cc/jayv/monopoly3/SpaceSelectionArea.java
@@ -61,30 +61,40 @@ public class SpaceSelectionArea implements ViewComponent {
         String baseMigSpec = "";
 
         // Left area, static labels
-        generateLabel(staticLabelID, LabelAlignment.RIGHT, true, "ID", "cell 0 0, align left");
-        generateLabel(staticLabelName, LabelAlignment.RIGHT, true, "Name", "cell 0 1, align left");
-        generateLabel(staticLabelIsOwned, LabelAlignment.RIGHT,  true, "Is owned", "cell 0 2, align left");
-        generateLabel(staticLabelOwnedBy, LabelAlignment.RIGHT, true, "Owned by", "cell 0 3, align left");
-        generateLabel(staticLabelSpaceType, LabelAlignment.RIGHT, true, "Space type", "cell 0 4, align left");
-        generateLabel(staticLabelLanded, LabelAlignment.RIGHT, true, "Landed", "cell 0 5, align left");
-        generateLabel(staticLabelPrice, LabelAlignment.RIGHT, true, "Price", "cell 0 6, align left");
+        generateLabel(staticLabelID, LabelAlignment.RIGHT, true, "ID", "cell 0 0, align right");
+        generateLabel(staticLabelName, LabelAlignment.RIGHT, true, "Name", "cell 0 1, align right");
+        generateLabel(staticLabelIsOwned, LabelAlignment.RIGHT,  true, "Is owned", "cell 0 2, align right");
+        generateLabel(staticLabelOwnedBy, LabelAlignment.RIGHT, true, "Owned by", "cell 0 3, align right");
+        generateLabel(staticLabelSpaceType, LabelAlignment.RIGHT, true, "Space type", "cell 0 4, align right");
+        generateLabel(staticLabelLanded, LabelAlignment.RIGHT, true, "Landed", "cell 0 5, align right");
+        generateLabel(staticLabelPrice, LabelAlignment.RIGHT, true, "Price", "cell 0 6, align right");
 
         // Left area, dynamic labels
-        generateLabel(labelID, LabelAlignment.LEFT, false, "ID", "cell 1 0, align left");
-        generateLabel(labelName, LabelAlignment.LEFT, false, "Name", "cell 1 1, align left");
-        generateLabel(labelIsOwned, LabelAlignment.LEFT,  false, "Is owned", "cell 1 2, align left");
-        generateLabel(labelOwnedBy, LabelAlignment.LEFT, false, "Owned by", "cell 1 3, align left");
-        generateLabel(labelSpaceType, LabelAlignment.LEFT, false, "Space type", "cell 1 4, align left");
-        generateLabel(labelLanded, LabelAlignment.LEFT, false, "Landed", "cell 1 5, align left");
-        generateLabel(labelPrice, LabelAlignment.LEFT, false, "Price", "cell 1 6, align left");
+        generateLabel(labelID, LabelAlignment.LEFT, false, "(no selection)", "cell 1 0, align left");
+        generateLabel(labelName, LabelAlignment.LEFT, false, "(no selection)", "cell 1 1, align left");
+        generateLabel(labelIsOwned, LabelAlignment.LEFT,  false, "(no selection)", "cell 1 2, align left");
+        generateLabel(labelOwnedBy, LabelAlignment.LEFT, false, "(no selection)", "cell 1 3, align left");
+        generateLabel(labelSpaceType, LabelAlignment.LEFT, false, "(no selection)", "cell 1 4, align left");
+        generateLabel(labelLanded, LabelAlignment.LEFT, false, "(no selection)", "cell 1 5, align left");
+        generateLabel(labelPrice, LabelAlignment.LEFT, false, "(no selection)", "cell 1 6, align left");
 
         // Right area, static labels
-        generateLabel(staticLabelRent, LabelAlignment.RIGHT, true, "Base rent", "cell 2 0, align left");
-        generateLabel(staticLabelRentHouse1, LabelAlignment.RIGHT, true, "+", "cell 2 1, align left", "/house-label-1-horiz-rightalign.png");
-        generateLabel(staticLabelRentHouse2, LabelAlignment.RIGHT, true, "+", "cell 2 2, align left", "/house-label-2-horiz-rightalign.png");
-        generateLabel(staticLabelRentHouse3, LabelAlignment.RIGHT, true, "+", "cell 2 3, align left", "/house-label-3-horiz-rightalign.png");
-        generateLabel(staticLabelRentHouse4, LabelAlignment.RIGHT, true, "+", "cell 2 4, align left", "/house-label-4-horiz-rightalign.png");
-        generateLabel(staticLabelRentHotel, LabelAlignment.RIGHT, true, "+", "cell 2 5, align left", "/hotel-label-1-horiz-rightalign.png");
+        generateLabel(staticLabelRent, LabelAlignment.RIGHT, true, "Base rent", "cell 2 0, align right");
+        generateLabel(staticLabelRentHouse1, LabelAlignment.RIGHT, true, "+", "cell 2 1, align right", "/house-label-1-horiz-rightalign.png");
+        generateLabel(staticLabelRentHouse2, LabelAlignment.RIGHT, true, "+", "cell 2 2, align right", "/house-label-2-horiz-rightalign.png");
+        generateLabel(staticLabelRentHouse3, LabelAlignment.RIGHT, true, "+", "cell 2 3, align right", "/house-label-3-horiz-rightalign.png");
+        generateLabel(staticLabelRentHouse4, LabelAlignment.RIGHT, true, "+", "cell 2 4, align right", "/house-label-4-horiz-rightalign.png");
+        generateLabel(staticLabelRentHotel, LabelAlignment.RIGHT, true, "+", "cell 2 5, align right", "/hotel-label-1-horiz-rightalign.png");
+        generateLabel(staticLabelCurrentRent, LabelAlignment.RIGHT, true, "Current rent", "cell 2 6, align right");
+
+        // Right area, dynamic labels
+        generateLabel(labelRent, LabelAlignment.LEFT, false, "(no selection)", "cell 3 0, align left");
+        generateLabel(labelRentHouse1, LabelAlignment.LEFT, false, "(no selection)", "cell 3 1, align left");
+        generateLabel(labelRentHouse2, LabelAlignment.LEFT, false, "(no selection)", "cell 3 2, align left");
+        generateLabel(labelRentHouse3, LabelAlignment.LEFT, false, "(no selection)", "cell 3 3, align left");
+        generateLabel(labelRentHouse4, LabelAlignment.LEFT, false, "(no selection)", "cell 3 4, align left");
+        generateLabel(labelRentHotel, LabelAlignment.LEFT, false, "(no selection)", "cell 3 5, align left");
+        generateLabel(labelCurrentRent, LabelAlignment.LEFT, false, "(no selection)", "cell 3 6, align left");
 
     }
 
