0630599cc6b633777d2a7976bba0b837900d90a9
jayvoigt <jayvoigt@iu.edu>
2022-04-13 13:58:27 -0400

    asset view for property groups working; added art assets

--- src/main/java/cc/jayv/monopoly3/ViewFrameInformationModule.java
+++ src/main/java/cc/jayv/monopoly3/ViewFrameInformationModule.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseListener;
 import java.io.IOException;
+import java.util.*;
 
 public class ViewFrameInformationModule {
 
@@ -34,6 +35,23 @@ public class ViewFrameInformationModule {
 
     JLabel staticLabelGOOJFCCount;
     JLabel labelGOOJFCCount;
+
+    JLabel labelOwnedColorBrown;
+    JLabel labelOwnedColorLightBlue;
+    JLabel labelOwnedColorMagenta;
+    JLabel labelOwnedColorOrange;
+    JLabel labelOwnedColorRed;
+    JLabel labelOwnedColorYellow;
+    JLabel labelOwnedColorGreen;
+    JLabel labelOwnedColorDarkBlue;
+
+    JLabel labelOwnedRailroad;
+    JLabel labelOwnedUtility;
+
+    Map<Color.colorGroupKeys, JLabel> ownedPropertyGroupLabels;
+
+    JLabel labelOwnedHouses;
+    JLabel labelOwnedHotels;
 
     Player player;
     Board board;
@@ -81,15 +99,40 @@ public class ViewFrameInformationModule {
         panelPlayerAssets.setSize(200, 150);
         panelPlayerAssets.setLayout(new MigLayout("ins 0"));
 
-        staticLabelOwnedPropertyCount = new JLabel("Owned:");
+        staticLabelOwnedPropertyCount = new JLabel("");
         staticLabelOwnedPropertyCount.setIcon(SwingHelper.getImageIconFromResource("/properties.png"));
         staticLabelOwnedPropertyCount.setToolTipText("Properties owned by the player.");
-        labelOwnedPropertyCount = new JLabel();
+        labelOwnedPropertyCount = new JLabel("0/28");
 
-        staticLabelGOOJFCCount = new JLabel("Owned:");
+        staticLabelGOOJFCCount = new JLabel("");
         staticLabelGOOJFCCount.setIcon(SwingHelper.getImageIconFromResource("/goojfc.png"));
         staticLabelGOOJFCCount.setToolTipText("Get Out of Jail Free Cards owned by the player.");
-        labelGOOJFCCount = new JLabel();
+        labelGOOJFCCount = new JLabel("0");
+
+        labelOwnedColorBrown = new JLabel(SwingHelper.getImageIconFromResource("/property-single-brown.png"));
+        labelOwnedColorLightBlue = new JLabel(SwingHelper.getImageIconFromResource("/property-single-lightblue.png"));
+        labelOwnedColorMagenta = new JLabel(SwingHelper.getImageIconFromResource("/property-single-magenta.png"));
+        labelOwnedColorOrange = new JLabel(SwingHelper.getImageIconFromResource("/property-single-orange.png"));
+        labelOwnedColorRed = new JLabel(SwingHelper.getImageIconFromResource("/property-single-red.png"));
+        labelOwnedColorYellow = new JLabel(SwingHelper.getImageIconFromResource("/property-single-yellow.png"));
+        labelOwnedColorGreen = new JLabel(SwingHelper.getImageIconFromResource("/property-single-green.png"));
+        labelOwnedColorDarkBlue = new JLabel(SwingHelper.getImageIconFromResource("/property-single-darkblue.png"));
+        labelOwnedRailroad = new JLabel(SwingHelper.getImageIconFromResource("/property-railroad.png"));
+        labelOwnedUtility = new JLabel(SwingHelper.getImageIconFromResource("/property-utility.png"));
+
+        ownedPropertyGroupLabels = new HashMap<>();
+        ownedPropertyGroupLabels.put(Color.colorGroupKeys.brown, labelOwnedColorBrown);
+        ownedPropertyGroupLabels.put(Color.colorGroupKeys.lightBlue, labelOwnedColorLightBlue);
+        ownedPropertyGroupLabels.put(Color.colorGroupKeys.magenta, labelOwnedColorMagenta);
+        ownedPropertyGroupLabels.put(Color.colorGroupKeys.orange, labelOwnedColorOrange);
+        ownedPropertyGroupLabels.put(Color.colorGroupKeys.red, labelOwnedColorRed);
+        ownedPropertyGroupLabels.put(Color.colorGroupKeys.yellow, labelOwnedColorYellow);
+        ownedPropertyGroupLabels.put(Color.colorGroupKeys.green, labelOwnedColorGreen);
+        ownedPropertyGroupLabels.put(Color.colorGroupKeys.darkBlue, labelOwnedColorDarkBlue);
+
+        labelOwnedHouses = new JLabel(SwingHelper.getImageIconFromResource("/house.png"));
+        labelOwnedHotels = new JLabel(SwingHelper.getImageIconFromResource("/hotel.png"));
+
     }
 
     private void arrangeComponentsPlayerStatus() {
@@ -107,11 +150,23 @@ public class ViewFrameInformationModule {
     }
 
     private void arrangeComponentsPlayerAssets() {
-        panelPlayerAssets.add(staticLabelOwnedPropertyCount, "cell 0 0");
+        panelPlayerAssets.add(staticLabelOwnedPropertyCount, "cell 0 0, split 4");
         panelPlayerAssets.add(labelOwnedPropertyCount, "cell 1 0");
 
-        panelPlayerAssets.add(staticLabelGOOJFCCount, "cell 0 1");
-        panelPlayerAssets.add(labelGOOJFCCount, "cell 1 1");
+        panelPlayerAssets.add(staticLabelGOOJFCCount, "cell 2 0");
+        panelPlayerAssets.add(labelGOOJFCCount, "cell 3 0");
+
+        panelPlayerAssets.add(labelOwnedColorBrown, "cell 0 3, split 4");
+        panelPlayerAssets.add(labelOwnedColorLightBlue, "cell 1 3");
+        panelPlayerAssets.add(labelOwnedColorMagenta, "cell 2 3");
+        panelPlayerAssets.add(labelOwnedColorOrange, "cell 3 3");
+        panelPlayerAssets.add(labelOwnedColorRed, "cell 0 4, split 4");
+        panelPlayerAssets.add(labelOwnedColorYellow, "cell 1 4");
+        panelPlayerAssets.add(labelOwnedColorGreen, "cell 2 4");
+        panelPlayerAssets.add(labelOwnedColorDarkBlue, "cell 3 4");
+        panelPlayerAssets.add(labelOwnedRailroad, "cell 0 5, split 4");
+        panelPlayerAssets.add(labelOwnedUtility, "cell 1 5");
+
     }
 
     public JPanel getPanelPlayerStatus() {
@@ -164,7 +219,9 @@ public class ViewFrameInformationModule {
                 }
             }
 
-            labelOwnedPropertyCount.setText(String.valueOf(ownedPropertyCount));
+            updatePropertyGroupOwnershipLabels();
+
+            labelOwnedPropertyCount.setText(String.valueOf(ownedPropertyCount) + "/28");
             labelGOOJFCCount.setText(String.valueOf(player.getGetOutOfJailFreeCardCount()));
 
         }
@@ -181,6 +238,43 @@ public class ViewFrameInformationModule {
         for (Component c : panelPlayerAssets.getComponents()) {
             c.setEnabled(false);
         }
+    }
+
+    private void updatePropertyGroupOwnershipLabels() {
+
+        JLabel label = null;
+
+        for ( Player p : board.players ) {
+            int ownedProperties = 0;
+            int totalProperties = 0;
+
+            if (p.getIsActive())  {
+                for (Color.colorGroupKeys colorGroup : Color.colorGroupKeys.values()) {
+
+                    label = ownedPropertyGroupLabels.get(colorGroup);
+
+                    totalProperties = board.getSpacesByColorGroup(colorGroup).size();
+                    for (Property property : board.getSpacesByOwnerID(p.getPlayerID())) {
+                        if (property instanceof Color c) {
+                            if (c.getColorGroup() == colorGroup) {
+                                ownedProperties++;
+                            }
+                        }
+                    }
+
+                    if (label != null) {
+                        label.setText(ownedProperties + "/" + totalProperties);
+
+                        // Indicate player has monopoly over property group
+                        if (ownedProperties == totalProperties) {
+                            label.setForeground(java.awt.Color.red);
+                        } else {
+                            label.setForeground(java.awt.Color.black);
+                        }
+                    }
+                }   // end for
+            }   // end if
+        }   // end for
     }
 
     public static void main(String args[]) {
